5443460977b411b57aa4f873ad3d949011a1aaa5
Athfan <aththaar47@gmail.com>
2021-12-23 07:18:18 +0530

    Version 1

---
+++ src/change.js
@@ -0,0 +1,7 @@
+export default function change() {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqSdWmM1LK5j4MM-NKFoChyI9L8Csu1dw",
+  authDomain: "blog-4c0f1.firebaseapp.com",
+  projectId: "blog-4c0f1",
+  storageBucket: "blog-4c0f1.appspot.com",
+  messagingSenderId: "920684333612",
+  appId: "1:920684333612:web:6b93513ab9066451c71f89"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
